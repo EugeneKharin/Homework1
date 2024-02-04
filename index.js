ce319c16daf56cdc9f5b3d5bbdb2fda1003e4152
Eugene <kharin.eugene@yandex.ru>
2024-02-04 21:35:44 +0800

    Убрал скобки. Дз №2

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 //Задание 1
-let a = (10);
+let a = 10;
 console.log(a)
 console.log(a += 10);
 //Задание 2
-let release = (2007);
+let release = 2007;
 console.log(release);
 //Задание 3
 let nameAuthor = 'Brendan Eich'
 console.log(nameAuthor);
 //Задание 4
-let ten = (10);
-let two = (2);
+let ten = 10;
+let two = 2;
 console.log(ten + two);
 console.log(ten - two);
 console.log(ten * two);
@@ -20,7 +20,7 @@ let five = 5;
 let resultTask5 = (two ** five); //Значение "2"(переменная "two") взято из задания 4 
 console.log(resultTask5);
 //Задание 6
-let nine = (9);
+let nine = 9;
 let resultTask6 = (nine % two);//Значение "2"(переменная "two") взято из задания 4 
 console.log(resultTask6);
 //Задание 7
@@ -38,14 +38,14 @@ console.log(ageTask8);
 //Задание 9
 const user = {
 name: 'Markus Alexej Persson',
-age: (44),
-isAdmin:(true),
+age: 44,
+isAdmin:true,
 };
 console.log(user);
 //Задание 9.1
 user ['city_of_residence'] = 'Stockholm';
 //Задание 9.2 
-user ['age'] = (90);
+user ['age'] = 90;
 //Задание 9.3
 delete user.city_of_residence;
 //Задание 9.4
