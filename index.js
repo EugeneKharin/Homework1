c3c2228f4534a592c75d0ef3a10eeb6f434e818e
Eugene <kharin.eugene@yandex.ru>
2024-02-12 22:38:28 +0800

    ДЗ №4

--- index.js
+++ index.js
@@ -1,101 +1,42 @@
 //Задание 1
-let password = 'пароль';
-let message = prompt('Введите пароль');
-if (message === 'пароль') {
-    console.log('Пароль введен верно')
-} else {
-    console.log('Пароль введен неправильно')
+let n = 0;
+while (n < 2) {
+    console.log('Привет');
+    n++;
 };
 //Задание 2
-let c = Number(prompt('Введите значение'));
-if (c >= 0 && c <= 10) {
-    console.log('Верно');
-} else {
-    console.log('Неверно');
-}
+let numbers = 1;
+while (numbers <= 5) {
+    console.log(numbers);
+    numbers++;
+};
 //Задание 3
-let d = Number(prompt('Введите значение'));
-let e = Number(prompt('Введите значение'));
-if (d > 100 || e > 100) {
-    console.log('Верно');
-} else {
-    console.log('Неверно');
-}
+let number = 7;
+while (number <= 22) {
+    console.log(number);
+    number++;
+};
 //Задание 4
-let a = '2';
-let b = '3';
-alert(+a + +b);
+const salary = {
+    Коля: 200,
+    Вася: 300,
+    Петя: 400
+
+};
+for (Коля in salary) {
+    console.log('Коля — зарплата ' + salary['Коля'] + ' долларов');
+};
+
 //Задание 5
-let winter = 'Зима';
-let spring = 'Весна';
-let summer = 'Лето';
-let autumn = 'Осень';
-let switchNumber = Number(prompt('Введите номер месяца'));
-switch (switchNumber) {
-    case (12):
-    case 1:
-    case 2:
-        console.log(winter);
-        break;
-    case 3:
-    case 4:
-    case 5:
-        console.log(spring);
-        break;
-    case 6:
-    case 7:
-    case 8:
-        console.log(summer);
-        break;
-    case 9:
-    case 10:
-    case 11:
-        console.log(autumn);
-        break;
-    default:
-        console.log('Неверно введён номер месяца');
-        break;
+let num = 0
+for (let numberN = 1000; numberN > 50; num++) {
+    numberN /= 2
+    console.log(num);
+    console.log(numberN);
 }
-//Задание 7
-let anyNumber = (prompt('Пожалуйста, введите любое число'))
-if (isNaN(anyNumber)) {
-    console.log(Number(anyNumber));
-} else {
-    if (anyNumber % 2 == 0) {
-        console.log('Чётное')
-    } else {
-        console.log('Нечётное')
-    }
+//Задание 6
 
-}
-//Задание 8
-let clienOS = 1 || 0
-if (clienOS === 1) {
-    console.log('Установите версию приложения для Android по ссылке')
-} else (clienOS === 0)
-{
-    console.log('Установите версию приложения для iOS по ссылке')
-}
-//Задание 9
-let clienOS2 = 1 || 0
-let year = 2015
-
-
-if (year <= 2015) {
-    if (clienOS === 1) {
-        console.log('Установите облегченную версию приложения для Android по ссылке')
-    } else (clienOS === 0)
-    console.log('Установите облегченную версию приложения для IOS по ссылке')
-    {
-
-    }
-} else {
-    if (clienOS === 1) {
-        console.log('Установите версию приложения для Android по ссылке')
-    } else (clienOS === 0)
-    console.log('Установите облегченную версию приложения для IOS по ссылке')
-    {
-
-    }
-
+for (let friday = 5; friday < 31; friday += 7) {
+    console.log(`Сегодня пятница, ${friday}-е число. Необходимо подготовить отчет.`)
+    
 }
